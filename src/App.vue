2f4a25a6bd5accae546373d1195c21b189b01af0
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-09-24 12:55:51 -0700

    feature: cleans up app and adds example list to test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <list >
+      <li>Some content</li>
+      <li>Some More Content</li>
+    </list>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/list'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+   List
   }
 }
 </script>

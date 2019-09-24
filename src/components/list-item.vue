3e9a64b034b112d0f01bbcf18edbd71a9d7d15bc
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-09-24 12:59:26 -0700

    feature: adds a list-item component
    This will serve as the base content for the list

---
+++ src/components/list-item.vue
@@ -0,0 +1,39 @@
+<template>
+    <li>
+        <button type="button" aria-labelledby="elementId" aria-describedby="dragDropHelpId"><slot name="button-content"></slot></button>
+        <slot name="list-content"  :id="elementId" >
+        </slot>
+    </li>
+</template>
+<script>
+let id = 0
+export default {
+    props: {
+        name: {
+            type: String,
+            default: 'list-element',
+            validator: function(value) {
+               return /$[\w-.]+$/.test(value)
+            }
+        },
+        
+        dragDropHelpId: {
+            type: String,
+            required: true,
+            validator: function(value) {
+                return /$[\w-.]+$/.test(value)
+            }
+        }
+    },
+    data() {
+        return {
+            idNum: ++id,
+        }
+    },
+    computed: {
+        elementId() {
+            return `${this.name}-${this.idNum}` 
+        }
+    },
+}
+</script>
